6bbe0dbc0aa6087e9caab484652eefc2412e5ed4
Empresszee <zainabbolanle2@gmail.com>
2022-09-08 19:12:48 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
-#include <stdio.h>
-
+#include <stdio.h>i
 /**
  *
- *  *main- main block
- *    *Return: 0
- *     */
-
+ *main- main bloc
+ */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
