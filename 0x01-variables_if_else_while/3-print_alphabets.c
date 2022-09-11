8b405e4e09733237dd478b8b2df4eb22e2d8c6e0
Empresszee <zainabbolanle2@gmail.com>
2022-09-11 14:31:31 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, and then in uppercase,
+ * followed by a new line.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	char mine;
+
+
+
+	for (mine = 'a'; mine <= 'z'; mine++)
+
+	{
+
+		putchar(mine);
+
+	}
+
+	for (mine = 'A'; mine <= 'Z'; mine++)
+
+	{
+
+		putchar(mine);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
