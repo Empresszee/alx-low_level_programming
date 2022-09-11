dfa5ea2f056fc7ab9015de5a4de603bd5fe12149
Empresszee <zainabbolanle2@gmail.com>
2022-09-11 14:46:58 -0700

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,43 @@
+nclude <stdio.h>
+
+/**
+ *
+ *  * main - prints the alphabet in lowercase
+ *
+ *   * Print all the letters except q and e,
+ *
+ *    * followed by a new line.
+ *
+ *     * Return: Always 0 (Success)
+ *
+ *      */
+
+int main(void)
+
+{
+
+		char mine = 'a';
+
+
+
+			while (mine <= 'z')
+
+					{
+
+								if (mine != 'e' && mine != 'q')
+
+											{
+
+															putchar(mine);
+
+																	}
+
+										mine++;
+
+											}
+
+				putchar('\n');
+
+					return (0);
+
+
