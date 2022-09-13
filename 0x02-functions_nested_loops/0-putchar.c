59e640f1ff6b85efd5771a6a2173a905d30f7f39
Empresszee <zainabbolanle2@gmail.com>
2022-09-13 16:13:05 -0700

    All 0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - this program prints _putchar
+ *
+ * Return:0 if succesful
+ */
+int main(void)
+{
+	char var[] = "_putchar";
+	int ii;
+	int lenVar = sizeof(var) / sizeof(var[0]);
+
+	for (ii = 0; ii < lenVar - 1; ii++)
+	{
+		_putchar(var[ii]);
+	}
+		_putchar('\n');
+
+		return(0);
+
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet -  prints the alphabet,
+ * in lowercase, followed by a new line.*
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+
+		int ii;
+
+		for (ii = 'a'; ii <= 'z'; ii++)
+			_putchar(ii);
+		_putchar('\n');
+
+}
