f48b4bcf1ff6fc25d78b65b9df8a1c261d7220e9
Empress <zainabbolanle2@gmail.com>
2022-09-26 15:53:36 -0700

    pointers

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,6 +11,7 @@ void print_diagsums(int *a, int size)
 	int i;
 
 	unsigned int sum, sum1;
+
 	sum = 0;
 	sum1 = 0;
 
@@ -19,5 +20,5 @@ void print_diagsums(int *a, int size)
 		sum += a[(size * i) + i];
 		sum1 += a[(size * (i + 1)) - (i + 1)];
 	}
-	printf("%d, %d\n", sum, sum1)
+	printf("%d, %d\n", sum, sum1);
 }
